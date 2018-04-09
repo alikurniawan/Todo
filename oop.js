c0d079a094dee476f20d86e4eec6f1f3bf7e065c
alikoerniawan <alikoernaiwan02@gmail.com>
2018-04-09 10:46:03 +0700

    Update Files

---
+++ oop.js
@@ -0,0 +1,32 @@
+class orang {
+  constructor(nama, gender){
+    this.nama = nama || "";
+    this.gender = gender || "";
+  }
+
+  gantiNama(namaBaru){
+    this.nama = namaBaru
+  }
+}
+
+var dono = new orang("john", "Male");
+
+john.nama
+
+/// ------------------------------------------------------
+class Guru extends orang{
+  constructor(nama, gender, gelar){
+    super(nama, gender)
+    this.gelar = []
+  }
+}
+tambahGelar(gelarBaru){
+  this.gelar.push(gelarBaru)
+}
+tampilkanNamaGelar(){
+  var text = this.nama
+  this.gelar.map((gelar) => {
+    text += `,${gelar}`
+    return text
+  })
+}
